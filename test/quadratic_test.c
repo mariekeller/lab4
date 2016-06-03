5812d73b5633bc66b9bf99058e4246f5153c533d
mariekeller <mariekeller357@gmail.com>
2016-06-04 02:08:54 +0600

    добавлено модульное тестирование

---
+++ test/quadratic_test.c
@@ -0,0 +1,69 @@
+﻿#include <ctest.h>
+#include "quadratic.h"
+
+CTEST(quadratic, zero)
+{
+    const float a = 0;
+    const float b = 1;
+    const float c = 2;
+	
+	float x1;
+	float x2;
+
+    const int res = solve(a, b, c, &x1, &x2);
+    
+    const int expected_res = 1;
+
+    ASSERT_EQUAL(expected_res, res);
+}
+
+CTEST(quadratic, two_roots)
+{
+    const float a = 1;
+    const float b = 10;
+    const float c = -24;
+	
+	float x1;
+	float x2;
+
+    const int res = solve(a, b, c, &x1, &x2);
+
+    const int expected_x1 = 2;
+	const int expected_x2 = -12;
+	
+    ASSERT_DBL_NEAR(expected_x1, x1);
+    ASSERT_DBL_NEAR(expected_x2, x2);
+}
+
+CTEST(quadratic, equal_roots)
+{
+    const float a = 1;
+    const float b = 6;
+    const float c = 9;
+	
+	float x1;
+	float x2;
+
+    const int res = solve(a, b, c, &x1, &x2);
+
+    const int expected_x1 = -3;
+	const int expected_x2 = -3;
+
+    ASSERT_DBL_NEAR(expected_x1, x1);
+}
+
+CTEST(quadratic, negative_discriminant)
+{
+    const float a = 2;
+    const float b = 1;
+    const float c = 2;
+	
+	float x1;
+	float x2;
+
+    const int res = solve(a, b, c, &x1, &x2);
+
+    const double expected_res = 2;
+
+    ASSERT_DBL_NEAR(expected_res, res);
+}
